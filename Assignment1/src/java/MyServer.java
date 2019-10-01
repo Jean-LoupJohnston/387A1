22b0d4981c478e0a0738fd74031ef25c48fcd7fa
Jean-Loup Johnston <jeanloupj94@gmail.com>
2019-10-01 18:50:37 -0400

    implemented httpserver

--- Assignment1/src/java/MyServer.java
+++ Assignment1/src/java/MyServer.java
@@ -1,10 +1,14 @@
 
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.InetSocketAddress;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -27,13 +31,47 @@ public class MyServer  {
     }
     
     private static void handleRequest(HttpExchange exchange) throws IOException {
-        String response = "Hi there!";
+        
+        String path = exchange.getRequestURI().getPath();
+        if(path.equals("/")) path = "index1.html";
+        String response = "";
+        try(FileInputStream input = new FileInputStream("C:\\Users\\Jean-Loup\\Documents\\NetBeansProjects\\Assignment1\\web\\"+path)) {
+
+        int data = input.read();
+        while(data != -1){
+            response += (char) data;
+            data = input.read();
+        }
+    }
+      
+        catch(FileNotFoundException e){
+                exchange.sendResponseHeaders(404, response.getBytes().length);
+        };
+        
+        if (path.substring(path.length()-4).equals("html"))
+        {
+            Headers headers = exchange.getResponseHeaders();
+             headers.add("Content-Type", "text/html");
+             exchange.sendResponseHeaders(200, response.getBytes().length);
+        }
+        
+        else if (path.substring(path.length()-3).equals("css"))
+        {
+            Headers headers = exchange.getResponseHeaders();
+             headers.add("Content-Type", "text/css");
+             exchange.sendResponseHeaders(200, response.getBytes().length);
+        }
         //response code and content length
-        exchange.sendResponseHeaders(200, response.getBytes().length);
+        
+        
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
 }
+
+    private static Reader NewFileReader(String path) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     
 }
